a81264aea7cc152aef02ddd4aec7721efbe455ec
xXTheSebXx <sebastianjerke@gmail.com>
2024-08-01 00:13:39 +0200

    fix typo

--- src/main/java/com/seb/Login/LoginPage.java
+++ src/main/java/com/seb/Login/LoginPage.java
@@ -12,17 +12,18 @@ public class LoginPage extends JavalinPage {
 
     public LoginPage(Context ctx) throws IOException {
         super(ctx);
+        String sessionid = ctx.cookie("JSESSIONID");
         ctx.sessionAttributeMap();
         String page = Files.readString(Path.of("html/login.html"));
         LoginStatus ls = LoginStatus.NONE;
-        if (ctx.cookie("JSESSIONID") != null) {
-            if (Main.sessionUserTimer.has(ctx.cookie("JSESSIONID")))
-                if (Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONID")).has("loginstatus")) {
-                    if (Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONID")).has("timestamp"))
-                        if (Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONID")).getLong("timestamp") < System.currentTimeMillis())
-                            Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONId")).put("loginstatus", LoginStatus.TIMED_OUT);
-                    ls = (LoginStatus) Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONID")).get("loginstatus");
-                    Main.sessionUserTimer.getJSONObject(ctx.cookie("JSESSIONID")).put("loginstatus", LoginStatus.NONE);
+        if (sessionid != null) {
+            if (Main.sessionUserTimer.has(sessionid))
+                if (Main.sessionUserTimer.getJSONObject(sessionid).has("loginstatus")) {
+                    if (Main.sessionUserTimer.getJSONObject(sessionid).has("timestamp"))
+                        if (Main.sessionUserTimer.getJSONObject(sessionid).getLong("timestamp") < System.currentTimeMillis())
+                            Main.sessionUserTimer.getJSONObject(sessionid).put("loginstatus", LoginStatus.TIMED_OUT);
+                    ls = (LoginStatus) Main.sessionUserTimer.getJSONObject(sessionid).get("loginstatus");
+                    Main.sessionUserTimer.getJSONObject(sessionid).put("loginstatus", LoginStatus.NONE);
                 }
         }
         switch (ls) {
